5042a6ecb22b2cfac8e93d9cd76e3553b554d5b9
ecabasso <eliahucabasso@gmail.com>
2025-12-24 15:24:36 -0600

    Simplified building process removing unnecesary class from setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,15 @@
 from setuptools import setup
+import pybind11
 from pybind11.setup_helpers import Pybind11Extension
 import os
-
-# A helper class to get pybind11 include paths
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include(self.user)
 
 ext_modules = [
     Pybind11Extension(
         'sde_simulator_cpp',
         ['src/main.cpp', 'src/simulator.cpp'],
         include_dirs=[
-            get_pybind_include(),
-            get_pybind_include(user=True),
+            pybind11.get_include(),
+            pybind11.get_include(True),
             os.path.abspath('src'),
         ],
         language='c++'
