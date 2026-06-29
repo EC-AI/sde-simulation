f5699c1ccabe7101c12bd9ab89a654f5e347e761
ecabasso <eliahucabasso@gmail.com>
2026-06-28 18:21:02 -0600

    Minor changes to sample script

--- tests/simulate.py
+++ tests/simulate.py
@@ -11,16 +11,14 @@ except ImportError:
 
 def main():
     # Simulation parameters
-    x0 = 3  # Initial X value
-    mu = 0.05   # Drift (5% per year)
-    sigma = 0.2 # Volatility (20% per year)
-    T = 10     # Time horizon (1 year)
-    n_steps = 252*T # Number of steps (e.g., trading days in a year)
+    x0 = 3     # Initial X value
+    T = 10     # Time horizon
+    n_steps = 252*T # Number of steps
 
     print("Running simulation with C++ backend...")
     
-    em = sde.euler_maruyama(x0, T=T, n_steps=n_steps, a=lambda x, t: (3-x)*t**2, b=lambda x, t: x**(1/2))
-    mil = sde.milstein(x0, T=T, n_steps=n_steps, a=lambda x, t: (3-x)*t**2, b=lambda x, t: x**(1/2), db_dx=lambda x, t: 0.5*(x)**(-.5))
+    em = sde.euler_maruyama(x0, T=T, n_steps=n_steps, a=lambda x, t: (3-x)*t**2, b=lambda x, t: x**(.5))
+    mil = sde.milstein(x0, T=T, n_steps=n_steps, a=lambda x, t: (3-x)*t**2, b=lambda x, t: x**(.5), db_dx=lambda x, t: 0.5*(x)**(-.5))
     print("Simulation complete.")
     print(type(em))
 
